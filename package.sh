473fdee0cf2a8016e084b266778aa0b2684b9dc8
Iain Murray <iain@iainmurray.net>
2020-07-28 20:48:33 +0100

    small fix to package.sh

--- package.sh
+++ package.sh
@@ -8,7 +8,7 @@ SRC=src
 # Set working directory to location of this script
 cd $(dirname $(readlink -m "$0"))
 
-VERSION=$(grep '"version":' src/manifest.json | sed 's/.*"\([0-9.]*\)".*/\1/')
+VERSION=$(grep '"version":' "$SRC"/manifest.json | sed 's/.*"\([0-9.]*\)".*/\1/')
 OUT="$EXTENSION"-"$VERSION"
 
 # Create .xpi zip file for firefox:
