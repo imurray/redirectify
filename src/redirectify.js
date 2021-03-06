4dce8e54aff1b47427daba374547a16022a528ff
Iain Murray <iain@iainmurray.net>
2021-03-06 10:53:12 +0000

    Multiple bypass domains (inspired by @mjkirk fork)
    Bypass original host and list provided. Allows bypassing for arbitrary
    numbers of separate overlays like inspirehep. For now, just added
    inspirehep, but expect to add to these.

--- src/redirectify.js
+++ src/redirectify.js
@@ -4,19 +4,19 @@
 // so often. The example redirect rules below each redirect a PDF to an HTML
 // index page from where clicking on the PDF will work.
 //
-// Each rule contains: [urlMatch, pattern, replace, (optional) bypassDomain]
+// Each rule contains: [urlMatch, pattern, replace, (optional list) bypassDomains]
 // - urlMatch in format at:
 //   https://developer.mozilla.org/en-US/Add-ons/WebExtensions/Match_patterns
 //   This match pattern must also be added to the permissions in the manifest.
 //   (Manually or with update_manifest_hosts.py)
 // - pattern and replace are used for regex rewriting of the URL
-// - rewriting is abandonded if a request is initiated at a subdomain of
-//   bypassDomain, or from the same domain as the requested URL.
+// - rewriting is abandoned if a request is initiated at a subdomain in
+//   bypassDomains, or from the same domain as the requested URL.
 //
-// Iain Murray, March 2018.
+// Iain Murray, March 2018, March 2021.
 
 RULES = [
-  ["*://*.arxiv.org/pdf/*", /(.*)\/pdf\/(.*?)(\.pdf)?$/, '$1/abs/$2'],
+  ["*://*.arxiv.org/pdf/*", /(.*)\/pdf\/(.*?)(\.pdf)?$/, '$1/abs/$2', ['.inspirehep.net']],
   ["*://xxx.lanl.gov/pdf/*", /(.*)\/pdf\/(.*?)(\.pdf)?$/, '$1/abs/$2'],
   ["*://arxiv-export-lb.library.cornell.edu/pdf/*", /(.*)\/pdf\/(.*?)(\.pdf)?$/, '$1/abs/$2'],
   ["*://*.openreview.net/pdf*", /pdf\?id=/, 'forum?id='],
@@ -24,10 +24,10 @@ RULES = [
   ["*://*.jmlr.org/papers/*.pdf", /\/papers\/volume(.*)\/[^\/]*\.pdf$/, '/papers/v$1.html'],
   ["*://jmlr.csail.mit.edu/papers/*.pdf", /\/papers\/volume(.*)\/[^\/]*\.pdf$/, '/papers/v$1.html'],
   ["*://delivery.acm.org/*", /.*delivery.acm.org\/[0-9.]*\/[0-9.]*\/([0-9]*)\/.*/,
-    'https://dl.acm.org/citation.cfm?id=$1', '.acm.org'],
+    'https://dl.acm.org/citation.cfm?id=$1', ['.acm.org']],
   ["*://papers.nips.cc/*.pdf", /\.pdf$/, ''],
   ["*://pdfs.semanticscholar.org/*", /.*lar.org\/([0-9a-f]{4})\/([0-9a-f]{36}).pdf/, 'https://www.semanticscholar.org/paper/$1$2',
-    '.semanticscholar.org'],
+    ['.semanticscholar.org']],
   ["*://*.biorxiv.org/content*", /((.*\/)biorxiv\/|)(.*)(\.full\.pdf)(\?.*)?$/, '$2$3']
 ];
 
@@ -38,8 +38,8 @@ function fix(request, pattern, replacement, bypassDomain) {
   //             Unfortunately .initiator is missing for new tabs, so in Chrome
   //             middle-clicking a PDF link on arXiv opens the abstract page.
   oldHost = new URL(request.originUrl || request.initiator || 'http://example.com').hostname;
-  newHost = bypassDomain || new URL(request.url).hostname;
-  if (! ('.' + oldHost).endsWith(newHost)) {
+  bypassDomains = (bypassDomain || []).concat(new URL(request.url).hostname);
+  if (! bypassDomains.some(x => ('.' + oldHost).endsWith(x))) {
     newUrl = request.url.replace(pattern, replacement);
     console.log('hello:', newUrl);
     if (newUrl != request.url)
