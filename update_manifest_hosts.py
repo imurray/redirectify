c9a0a37a3979acdbeecaa058e4e5b3d396c1a438
Iain Murray <iain@iainmurray.net>
2021-03-07 09:45:37 +0000

    Add permissions needed by Chrome.

--- update_manifest_hosts.py
+++ update_manifest_hosts.py
@@ -5,19 +5,27 @@
 # Extracts hosts used in RULES and injects them into the permissions block in
 # the manifest. Can use it to help update the manifest when adding new rules.
 
-import os
+import os, re, collections
 
 DIR = os.path.join(os.path.realpath(os.path.dirname(__file__)), 'src')
 
-# Find match patterns from code
-hosts = []
+# Grab rules from javascript code
 with open(os.path.join(DIR, 'redirectify.js'), 'r') as fid:
-    for line in fid:
-        if line.startswith('  ["*://'):
-            host_line = ' '*4 + line[len('  ['):line.index(', /')+1] + '\n'
-            if host_line not in hosts:
-                # this code has quadratic complexity. But so few lines, not worth fiddling with set/dict
-                hosts.append(host_line)
+    all_code = fid.read()
+rules_txt = re.search(r'RULES = \[(.*?)\];\n', all_code, re.DOTALL).groups()[0]
+
+# Find match patterns needed by rules (fragile, not parsing rigorously)
+hosts = []
+for rule_txt in rules_txt.strip().strip(']').split('],'):
+    match = rule_txt.split(',')[0].strip().strip('[')
+    hosts.append(' '*4 + match + ",\n")
+    # origin of request needs host permission in Chrome (could omit for Firefox)
+    final_part = rule_txt.split('[')[-1].strip()
+    if final_part.endswith(']'):
+        for host in final_part.strip(']').split(','):
+            hosts.append(' '*4 + '"*://*' + host.strip()[1:-1] + '/*",\n')
+# deduplicate:
+hosts = list(collections.OrderedDict.fromkeys(hosts))
 
 # Get other bits of existing manifest
 bits1 = []
